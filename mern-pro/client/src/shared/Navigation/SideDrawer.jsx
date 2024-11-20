72bb69cf0fa699b94a9d325f33bc2dd7bc78660b
SDQ-BQA <sdmbqa420@gmail.com>
2024-11-21 00:49:54 +0530

    protal-setup

--- mern-pro/client/src/shared/Navigation/SideDrawer.jsx
+++ mern-pro/client/src/shared/Navigation/SideDrawer.jsx
@@ -1,7 +1,10 @@
 import './SideDrawer.css'
+import ReactDom from 'react-dom'
 
 const SideDrawer = (props) => {
-  return<aside className="side-drawer">{props.children}</aside>
+  const content = <aside className="side-drawer">{props.children}</aside>
+
+  return ReactDom.createPortal(content, document.getElementById("drawer-hook"))
 }
 
 export default SideDrawer
