c14f416c0f1cb6e601ce70f5b8f66482580050b6
SDQ-BQA <sdmbqa420@gmail.com>
2024-12-04 00:35:17 +0530

    context concept

--- mern-pro/client/src/App.jsx
+++ mern-pro/client/src/App.jsx
@@ -11,34 +11,48 @@ import MainNavigation from "./shared/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import Auth from "./user/pages/Auth";
+import { AuthContext } from "./shared/context/auth-context";
+import { useCallback, useState } from "react";
 
 function App() {
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setisLoggedIn(true);
+  }, []);
+  const logout = useCallback(() => {
+    setisLoggedIn(false);
+  }, []);
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        {/* using switch so if one path match, then only particular component will execute not the remaining part code */}
-        <Switch>
-          <Route path="/" exact>
-            <Users />
-          </Route>
-          <Route path="/places/new" exact>
-            <NewPlace />
-          </Route>
-          <Route path="/:userId/places" exact>
-            <UserPlaces />
-          </Route>
-          <Route path="/places/:placeId" exact>
-            <UpdatePlace />
-          </Route>
-          <Route path="/auth" exact>
-            <Auth />
-          </Route>
-          {/* If any path is not matching */}
-          <Redirect to="/" />
-        </Switch>
-      </main>
-    </Router>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>
+          {/* using switch so if one path match, then only particular component will execute not the remaining part code */}
+          <Switch>
+            <Route path="/" exact>
+              <Users />
+            </Route>
+            <Route path="/places/new" exact>
+              <NewPlace />
+            </Route>
+            <Route path="/:userId/places" exact>
+              <UserPlaces />
+            </Route>
+            <Route path="/places/:placeId" exact>
+              <UpdatePlace />
+            </Route>
+            <Route path="/auth" exact>
+              <Auth />
+            </Route>
+            {/* If any path is not matching */}
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
