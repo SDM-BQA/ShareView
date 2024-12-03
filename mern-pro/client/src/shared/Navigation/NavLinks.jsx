d6fd7f59898bc49c80e85bee66c263c0d6e5aff7
SDQ-BQA <sdmbqa420@gmail.com>
2024-12-04 00:38:02 +0530

    listening to context

--- mern-pro/client/src/shared/Navigation/NavLinks.jsx
+++ mern-pro/client/src/shared/Navigation/NavLinks.jsx
@@ -1,23 +1,28 @@
-import { NavLink } from 'react-router-dom/cjs/react-router-dom.min'
-import './NavLinks.css'
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import "./NavLinks.css";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth-context";
 
 const NavLinks = (props) => {
+  const auth = useContext(AuthContext);
   return (
-    <ul className='nav-links'>
-        <li>
-            <NavLink to={"/"} exact>ALL USERS</NavLink>
-        </li>
-        <li>
-            <NavLink to={"/u1/places"}>MY PLACES</NavLink>
-        </li>
-        <li>
-            <NavLink to={"/places/new"}>ADD PLACES</NavLink>
-        </li>
-        <li>
-            <NavLink to={"/auth"}>AUTHENTICATE</NavLink>
-        </li>
+    <ul className="nav-links">
+      <li>
+        <NavLink to={"/"} exact>
+          ALL USERS
+        </NavLink>
+      </li>
+      {auth.isLoggedIn && <li>
+        <NavLink to={"/u1/places"}>MY PLACES</NavLink>
+      </li>}
+      {auth.isLoggedIn && <li>
+        <NavLink to={"/places/new"}>ADD PLACES</NavLink>
+      </li>}
+      {!auth.isLoggedIn && <li>
+        <NavLink to={"/auth"}>AUTHENTICATE</NavLink>
+      </li>}
     </ul>
-  )
-}
+  );
+};
 
-export default NavLinks
+export default NavLinks;
