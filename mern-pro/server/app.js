e11c977e7ea9062542e14d726cbdcf284e03cadc
SDQ-BQA <sdmbqa420@gmail.com>
2024-12-04 17:44:54 +0530

    setup auth route

--- mern-pro/server/app.js
+++ mern-pro/server/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+
 const HttpError = require('./models/http-error')
 
 const app = express();
@@ -11,6 +13,9 @@ app.use(bodyParser.json());
 
 // places Routes
 app.use("/api/places", placesRoutes); // => /api/places/...
+
+// users route
+app.use("/api/users", usersRoutes)
 
 //
 app.use((req,res,next)=>{
