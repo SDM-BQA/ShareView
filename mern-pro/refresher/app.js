e28dae295aa708818bf2a599f3127f076bb27138
SDQ-BQA <sdmbqa420@gmail.com>
2024-12-06 00:16:50 +0530

    post & get data

--- mern-pro/refresher/app.js
+++ mern-pro/refresher/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const monogoPrac = require('./mongo')
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/products');
+app.post('/products', monogoPrac.createProduct);
 
-app.get('/products');
+app.get('/products', monogoPrac.getProducts);
 
-app.listen(3000);
+app.listen(5000,()=>{
+    console.log("Listening on port 5000");
+    
+});
--- mern-pro/refresher/mongo.js
+++ mern-pro/refresher/mongo.js
@@ -1,14 +1,42 @@
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require("mongodb").MongoClient;
 
-const url = 'mongodb+srv://sdm:QylLr34vHGsSnEJu@cluster0.hr1g3.mongodb.net/products?retryWrites=true&w=majority&appName=Cluster0'
+const url =
+  "mongodb+srv://sdm:QylLr34vHGsSnEJu@cluster0.hr1g3.mongodb.net/products_test?retryWrites=true&w=majority&appName=Cluster0";
 
 const createProduct = async (req, res, next) => {
-    
-}
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+  const client = new MongoClient(url);
+  let products;
+  try {
+    await client.connect(); // Connect to the database
+    const db = client.db(); // Access the database
+    const result = await db.collection("products").insertOne(newProduct); // Await the insert operation
+    console.log(result);
+    res.json({ message: "Product created successfully", product: newProduct });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "We could not store data" });
+  } finally {
+    await client.close(); // Ensure the client is closed in the `finally` block
+  }
+};
 
 const getProducts = async (req, res, next) => {
-    
-}
+  const client = new MongoClient(url);
+  try {
+    await client.connect();
+    const db = client.db();
+    products = await db.collection("products").find().toArray();
+    res.json(products);
+  } catch (error) {
+    return res.json({ message: "Could not retrieve products" });
+  } finally {
+    await client.close(); // Ensure the client is closed in the `finally` block
+  }
+};
 
-exports.createProduct = createProduct
-exports.getProducts = getProducts
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
