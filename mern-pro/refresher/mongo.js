c2a01e0f3765b4da9fd62bd5534353fd7b1d61c1
SDQ-BQA <sdmbqa420@gmail.com>
2024-12-05 21:36:12 +0530

    monogo_db setup

---
+++ mern-pro/refresher/mongo.js
@@ -0,0 +1,14 @@
+const MongoClient = require('mongodb').MongoClient
+
+const url = 'mongodb+srv://sdm:QylLr34vHGsSnEJu@cluster0.hr1g3.mongodb.net/products?retryWrites=true&w=majority&appName=Cluster0'
+
+const createProduct = async (req, res, next) => {
+    
+}
+
+const getProducts = async (req, res, next) => {
+    
+}
+
+exports.createProduct = createProduct
+exports.getProducts = getProducts
