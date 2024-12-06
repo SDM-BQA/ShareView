cb47a0b60ecd181e7a2f0fc36248b681a81b44d2
SDQ-BQA <sdmbqa420@gmail.com>
2024-12-06 20:55:20 +0530

    add login db

--- mern-pro/server/controllers/users-controller.js
+++ mern-pro/server/controllers/users-controller.js
@@ -71,7 +71,7 @@ const signup = async (req, res, next) => {
 };
 
 // login
-const login = (req, res, next) => {
+const login =async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors);
@@ -80,13 +80,29 @@ const login = (req, res, next) => {
   }
   const { email, password } = req.body;
 
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError(
-      "Could not identify user, credentials seem to be wrong",
-      401
-    );
+  // const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError("Login Fail, please try again", 500);
+    return next(error);
   }
+
+  try{
+    if (!existingUser || existingUser.password !== password) {
+      throw new HttpError(
+        "Could not identify user, credentials seem to be wrong",
+        401
+      );
+    }
+  }catch(err){
+    const error = new HttpError("Login Fail, please try again", 500);
+    return next(error);
+    
+  }
+
 
   res.json({ message: "Logged in!" });
 };
