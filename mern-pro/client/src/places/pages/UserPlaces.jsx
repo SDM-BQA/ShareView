d62b96a17414ead1ee48662dc57cae614eb60d25
SDQ-BQA <sdmbqa420@gmail.com>
2024-12-08 17:27:04 +0530

    get place by userId

--- mern-pro/client/src/places/pages/UserPlaces.jsx
+++ mern-pro/client/src/places/pages/UserPlaces.jsx
@@ -1,43 +1,46 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import PlaceList from "../components/PlaceList";
 
-
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import { useEffect, useState } from "react";
+import ErrorModal from "../../shared/UIElement/ErrorModal";
+import LoadingSpinner from "../../shared/UIElement/LoadingSpinner";
 
 const UserPlaces = (props) => {
-  const DUMMY_PLACES = [
-    {
-      id: "p1",
-      title: "Tajmahal",
-      description: "7 Wonder's of the world",
-      imageUrl:
-        "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcSNULb-ekVZasVOCr_y4-HmpFhcothsGWvWFuYbQxhn_t_DCZxaL14mzAGDmpGcU8q15Ah1bbU7vxclTAVq3Mft-vbTe3uvvIyzqjCCsD8",
-      address: "Dharmapuri Forest Colony Tajganj Agra Uttar Pradesh 282001",
-      location: {
-        lat: 27.1751,
-        lng: 78.0421,
-      },
-      creatorId: "u1",
-    },
-    {
-      id: "p2",
-      title: "Tmahal",
-      description: "7 Wonder's of the world",
-      imageUrl:
-        "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcSNULb-ekVZasVOCr_y4-HmpFhcothsGWvWFuYbQxhn_t_DCZxaL14mzAGDmpGcU8q15Ah1bbU7vxclTAVq3Mft-vbTe3uvvIyzqjCCsD8",
-      address: "Dharmapuri Forest Colony Tajganj Agra Uttar Pradesh 282001",
-      location: {
-        lat: 27.1751,
-        lng: 78.0421,
-      },
-      creatorId: "u2",
-    },
-  ];
+  const [loadedPlaces, setLoadedPlaces] = useState(null);
+
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
   const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter(
-    (place) => place.creatorId === userId
-  );
-  return <PlaceList items={loadedPlaces} />;
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/places/user/${userId}`
+        );
+        setLoadedPlaces(responseData.places);
+        console.log(responseData.places);
+        
+      } catch (err) {
+        console.log(err);
+        setLoadedPlaces(null)
+      }
+    };
+    fetchPlaces()
+  }, [sendRequest, userId]);
+
+  return (
+  <>
+  {/* <ErrorModal error={error} onClear={clearError}/> */}
+  {isLoading && (
+    <div className="center">
+      <LoadingSpinner/>
+    </div>
+  )}
+  {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />};
+  </>
+  )
 };
 
 export default UserPlaces;
